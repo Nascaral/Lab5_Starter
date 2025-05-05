3f3ac539a92466a66e87133839baa16eec076bbb
Nascaral <nasser.ajobs@gmail.com>
2025-05-05 15:16:38 -0700

    testing true

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,9 +1,9 @@
 // sum.test.js
 test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(4);
+  expect(1 + 2).toBe(3);
 });
 
 import { sum } from '../code-to-unit-test/sum.js';
 test('sum(1,2) returns 3', () => {
-  expect(sum(1,2)).toBe(4);
+  expect(sum(1,2)).toBe(3);
 });
